97080995944aff9ff7f550c95a8325fe25804e55
RabbitSnore <timothy.luke@psy.gu.se>
2021-01-26 19:38:33 +0100

    getting distracted by Darley et al if we're being honest

--- scripts/darley_core.R
+++ scripts/darley_core.R
@@ -4,3 +4,28 @@
 # Darley, Carlsmith, & Robinson (2000) [Study 2] -- Core Analyses
 
 ######################################################################
+
+# HYPOTHESIS 1 -------------------------------------------------------
+
+# HYPOTHESIS 1 -------------------------------------------------------
+
+# HYPOTHESIS 1 -------------------------------------------------------
+
+# MANIPULATION CHECK 1 -----------------------------------------------
+
+# Participants will select imprisonment at higher rates when the perpetrator acted in a jealous rage, compared to when the behavior was caused by (1) an inoperable tumor or (2) an operable tumor.
+
+# Proportion differences (odds ratios) in selecting Imprisonment, for Scenario
+
+# MANIPULATION CHECK 2 -----------------------------------------------
+
+# Participants will select hospitalization at higher rates when the perpetrator acted because of an inoperable tumor, compared to when the behavior was caused by a jealous rage.
+# Darley et al found a nonsignificant difference in preferring hospitalization between operable and inoperable tumors. We will assess this difference as well.
+
+# Proportion differences (odds ratios) in selecting Hospitalization, for Scenario
+
+# MANIPULATION CHECK 3 -----------------------------------------------
+
+# Participants will select release at higher rates when the perpetrator acted because of an operable tumor that has now been treated, compared to when the behavior was caused by (1) an inoperable tumor or (2) a jealous rage.
+
+# Proportion differences (odds ratios) in selecting Release, for Scenario
