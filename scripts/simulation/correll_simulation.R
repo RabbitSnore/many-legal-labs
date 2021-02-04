b5560124a8a83af9077e44cd23d37207b370c8a8
RabbitSnore <rabbitsnore@gmail.com>
2021-02-04 13:38:18 +0100

    Correll simulation up and running kind of

--- scripts/simulation/correll_simulation.R
+++ scripts/simulation/correll_simulation.R
@@ -8,26 +8,34 @@
 # Overview ------------------------------------------------------------
 
 ## Each participant performs 80 trials, with 20 allocated per cell in the 2 x 2 design
-## The factors are race (White/Black) and object (Gun/No Gun)
-## Each trial has a reaction time (in milliseconds) and an accuracy indicator (correct/incorrect)
+## The factors are race (White/Black) and object (Armed/Unarmed)
+## Each trial has a response latency (in milliseconds) and an accuracy indicator (correct/incorrect)
 
 # Set up environment --------------------------------------------------
 
 ## Packages
 
-packages <- c("dplyr")
+packages <- c("dplyr", "tidyr", "stringr")
 
 lapply(packages, library, character.only = TRUE)
+
+## Seed
+
+set.seed(42069)
 
 # Set up design--------------------------------------------------------
 
 ## Basics of multi-lab design and study design
 
-n_labs   <- 55 # Number of labs
-n_per    <- 80 # Number of participants per lab
+n_labs    <- 55 # Number of labs
+n_per     <- 80 # Number of participants per lab
 
-k_cells  <- 4  # Number of cells in the design
-n_trials <- 20 # Number of trials per cell
+race      <- c("white", "black")    # Race variable labels
+object    <- c("armed", "unarmed")  # Object variable labels
+n_trials  <- 20                     # Number of trials per cell
+
+trials    <- str_pad(1:n_trials, nchar(n_trials), pad = 0) # Labels for trials
+variables <- c("latency", "accuracy")                      # Labels for variables
 
 ## Set up participants
 
@@ -35,16 +43,52 @@ correll_wide <-
   expand.grid(
     lab     = 1:n_labs,
     subject = 1:n_per
-  ) %>% 
+    ) %>% 
   arrange(by = lab) %>% 
   mutate(
     subject = paste(lab, subject, sep = "_")
   )
 
+## Set up data column names
+
+trial_combn <- 
+  expand.grid(
+    race   = race, 
+    object = object, 
+    trial  = trials,
+    variable = variables
+    )
+
+trial_cols <- apply(trial_combn, 1, paste, collapse = "_")
+
 # Simulate wide-form data ---------------------------------------------
 
+## Generate response latencies from a Poisson distribution
 
+latencies  <- replicate(n_trials * nrow(expand.grid(race, object)), rpois(nrow(correll_wide), 500)) %>% 
+  as.data.frame()
 
-# Wrangle to long-form ------------------------------------------------
+## Generate accuracy indicators from a binomial distribution
 
+accuracies <- replicate(n_trials * nrow(expand.grid(race, object)), rbinom(nrow(correll_wide), 1, .95)) %>% 
+  as.data.frame()
 
+## Bind the simulated data and rename the columns
+
+response_data <- bind_cols(latencies, accuracies)
+
+colnames(response_data) <- trial_cols
+
+correll_wide <- bind_cols(correll_wide, response_data)
+
+# Save data simulated data file ---------------------------------------
+
+if (!file.exists("./data/correll_sim.csv")) {
+  
+  write.csv(
+    correll_wide,
+    "./data/correll_sim.csv",
+    row.names = FALSE
+  )
+  
+}
