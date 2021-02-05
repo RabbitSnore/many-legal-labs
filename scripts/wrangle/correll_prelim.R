11700483ef2a24b7d4ef0ea7fcde1da29f6b22a5
Lina Nyström <lina.nystrom@psy.gu.se>
2021-02-05 16:06:59 +0100

    types of error per trial

--- scripts/wrangle/correll_prelim.R
+++ scripts/wrangle/correll_prelim.R
@@ -41,12 +41,33 @@ correll_wrangle <- read.csv("./data/correll_sim.csv")
 
 correll_latencies <- select(correll_wrangle, contains("latency"))
 
-correll_latencies[correll_latencies > 850] <- NA
+exclusion_lat_wa <- sum(select(correll_latencies, contains ("white_armed")) > 850)
 
-white_armed <- select(correll_latencies, contains("white_armed"))
-black_armed <- select(correll_latencies, contains("black_armed"))
-white_unarmed <- select(correll_latencies, contains("white_unarmed"))
-black_unarmed <- select(correll_latencies, contains("black_unarmed"))
+exclusion_lat_ba <- sum(select(correll_latencies, contains ("black_armed")) > 850)
+
+exclusion_lat_wu <- sum(select(correll_latencies, contains("white_unarmed")) > 850)
+
+exclusion_lat_bu <- sum(select(correll_latencies, contains("black_unarmed")) > 850)
+
+
+correll_latencies[correll_latencies > 850] <- NA
+exclusion_lat <- sum(is.na(correll_latencies))
+
+
+correll_accuracy <- select(correll_wrangle, contains("accuracy"))
+correll_latencies[correll_accuracy == 0] <- NA
+exclusion_acc <- sum(correll_accuracy == 0)
+
+exclusion_acc_wa <- sum(select(correll_accuracy, contains ("white_armed")) == 0)
+exclusion_acc_ba <- sum(select(correll_accuracy, contains ("black_armed")) == 0)
+exclusion_acc_wu <- sum(select(correll_accuracy, contains ("white_unarmed")) == 0)
+exclusion_acc_bu <- sum(select(correll_accuracy, contains ("black_unarmed")) == 0)
+
+
+white_armed <- white_armed %>% mutate(white_armed_lat_mean = rowMeans(white_armed, na.rm = TRUE))
+black_armed <- black_armed %>% mutate(black_armed_lat_mean = rowMeans(black_armed, na.rm = TRUE))
+white_unarmed <- white_unarmed %>% mutate(white_unarmed_lat_mean = rowMeans(white_unarmed, na.rm = TRUE))
+black_unarmed <- black_unarmed %>% mutate(black_unarmed_lat_mean = rowMeans(black_unarmed, na.rm = TRUE))
 
 
 ## Save data
