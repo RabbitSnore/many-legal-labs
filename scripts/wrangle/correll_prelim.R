a62a4807ea9de1004d55dd179a07655579ceac71
Lina Nyström <lina.nystrom@psy.gu.se>
2021-02-05 17:45:21 +0100

    Log transform response latencies

--- scripts/wrangle/correll_prelim.R
+++ scripts/wrangle/correll_prelim.R
@@ -52,16 +52,28 @@ exclusion_lat_bu <- sum(select(correll_latencies, contains("black_unarmed")) > 8
 
 correll_latencies[correll_latencies > 850] <- NA
 exclusion_lat <- sum(is.na(correll_latencies))
+sum_lat_exclusions <- rowSums(is.na(correll_latencies))
 
 
 correll_accuracy <- select(correll_wrangle, contains("accuracy"))
 correll_latencies[correll_accuracy == 0] <- NA
+
+correll_latencies <- log(correll_latencies)
+
 exclusion_acc <- sum(correll_accuracy == 0)
+sum_acc_exclusions <- rowSums((correll_accuracy == 0))
+
+sum_exclusions <- rowSums(is.na(correll_latencies))
 
 exclusion_acc_wa <- sum(select(correll_accuracy, contains ("white_armed")) == 0)
 exclusion_acc_ba <- sum(select(correll_accuracy, contains ("black_armed")) == 0)
 exclusion_acc_wu <- sum(select(correll_accuracy, contains ("white_unarmed")) == 0)
 exclusion_acc_bu <- sum(select(correll_accuracy, contains ("black_unarmed")) == 0)
+
+white_armed <- select(correll_latencies, contains ("white_armed"))
+black_armed <- select(correll_latencies, contains ("black_armed"))
+white_unarmed <- select(correll_latencies, contains ("white_unarmed"))
+black_unarmed <- select(correll_latencies, contains ("black_unarmed"))
 
 
 white_armed <- white_armed %>% mutate(white_armed_lat_mean = rowMeans(white_armed, na.rm = TRUE))
@@ -69,6 +81,12 @@ black_armed <- black_armed %>% mutate(black_armed_lat_mean = rowMeans(black_arme
 white_unarmed <- white_unarmed %>% mutate(white_unarmed_lat_mean = rowMeans(white_unarmed, na.rm = TRUE))
 black_unarmed <- black_unarmed %>% mutate(black_unarmed_lat_mean = rowMeans(black_unarmed, na.rm = TRUE))
 
+
+## Smushing code
+
+correll_latencies$sum_lat_exclusions <- sum_lat_exclusions
+correll_latencies$sum_acc_exclusions <- sum_acc_exclusions
+correll_latencies$sum_exclusions <- sum_exclusions
 
 ## Save data
 
