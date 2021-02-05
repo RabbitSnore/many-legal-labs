4bbb5eb90b4e90559912c2ef4d569cb0e6e4e294
Lina Nyström <lina.nystrom@psy.gu.se>
2021-02-05 14:22:41 +0100

    Remove trials that exceed 850ms

--- scripts/wrangle/correll_prelim.R
+++ scripts/wrangle/correll_prelim.R
@@ -24,3 +24,35 @@
 # Calculate within participant cell means for response latencies
 
 # Calculate errors for each cell, within participants
+
+
+
+## Packages
+
+packages <- c("dplyr", "stringr")
+
+lapply(packages, library, character.only = TRUE)
+
+## Import
+
+correll_wrangle <- read.csv("./data/correll_sim.csv")
+
+## Exclusions
+
+correll_latencies <- select(correll_wrangle, contains("latency"))
+
+correll_latencies[correll_latencies > 850] <- NA
+
+white_armed <- select(correll_latencies, contains("white_armed"))
+black_armed <- select(correll_latencies, contains("black_armed"))
+white_unarmed <- select(correll_latencies, contains("white_unarmed"))
+black_unarmed <- select(correll_latencies, contains("black_unarmed"))
+
+
+## Save data
+
+write.csv(
+  correll_wrangle,
+  "./data/correll_wrangle.csv",
+  row.names = FALSE
+)
