1f5eed01e721f54187652bea945232af7a0704d7
RabbitSnore <rabbitsnore@gmail.com>
2021-02-19 12:25:20 +0100

    data exportation

--- scripts/calculate/darley_core.R
+++ scripts/calculate/darley_core.R
@@ -119,7 +119,7 @@ for (i in 1:lab_count_darley) {
 
 # MANIPULATION CHECK 1 ------------------------------------------------
 
-# Participants will attribute responsibility to the actor more frequently in the jealous rage condition, compared to when the behavior was caused by (1) an inoperable tumor or (2) an operable tumor.
+# Participants will attribute responsibility to the actor more frequently in the jealous rage condition, compared to when the behavior was caused by an inoperable tumor.
 
 # Proportion differences (odds ratios) in attributing responsibility to the actor, for Scenario (jealous rage vs. inoperable and operable.)
 
@@ -127,15 +127,13 @@ for (i in 1:lab_count_darley) {
 
 ### Set up empty data frames for effects
 
-darley_mc1a_lor <- empty_lor_data(lab_count_darley)
-
-darley_mc1b_lor <- empty_lor_data(lab_count_darley)
+darley_mc1_lor <- empty_lor_data(lab_count_darley)
 
 ### Compute log odds ratios for each lab
 
 for (i in 1:lab_count_darley) {
   
-  darley_mc1a_lor[i, ] <- odds_calc(
+  darley_mc1_lor[i, ] <- odds_calc(
     
     ID = unique(darley$lab)[i], 
     
@@ -149,28 +147,11 @@ for (i in 1:lab_count_darley) {
   
 }
 
-for (i in 1:lab_count_darley) {
-  
-  darley_mc1b_lor[i, ] <- odds_calc(
-    
-    ID = unique(darley$lab)[i], 
-    
-    x = darley$scenario[darley$lab == i], 
-    y = darley$attribution[darley$lab == i], 
-    
-    cond_1 = "jealous_rage", 
-    cond_2 = "operable"
-    
-  )
-  
-}
+# MANIPULATION CHECK 2 ------------------------------------------------
 
-# MANIPULATION CHECK 2 -----------------------------------------------
+# Participants will attribute responsibility to the actor more frequently in the jealous rage condition, compared to when the behavior was caused by an inoperable tumor.
 
-# Participants will attribute responsibility to the actor more frequently in the inoperable tumor condition compared to the operable tumor condition.
-# Darley et al. found a nonsignificant difference for this comparison, but the direction of proportions was such that more people attributed responsibility to the actor when the tumor was inoperable.
-
-# Proportion difference (odds ratios) in attributing responsibility to the actor, for Scenario (operable vs. inoperable)
+# Proportion differences (odds ratios) in attributing responsibility to the actor, for Scenario (jealous rage vs. inoperable and operable.)
 
 ## Calculate effect sizes 
 
@@ -189,9 +170,123 @@ for (i in 1:lab_count_darley) {
     x = darley$scenario[darley$lab == i], 
     y = darley$attribution[darley$lab == i], 
     
+    cond_1 = "jealous_rage", 
+    cond_2 = "operable"
+    
+  )
+  
+}
+
+
+# MANIPULATION CHECK 3 -----------------------------------------------
+
+# Participants will attribute responsibility to the actor more frequently in the inoperable tumor condition compared to the operable tumor condition.
+# Darley et al. found a nonsignificant difference for this comparison, but the direction of proportions was such that more people attributed responsibility to the actor when the tumor was inoperable.
+
+# Proportion difference (odds ratios) in attributing responsibility to the actor, for Scenario (operable vs. inoperable)
+
+## Calculate effect sizes 
+
+### Set up empty data frames for effects
+
+darley_mc3_lor <- empty_lor_data(lab_count_darley)
+
+### Compute log odds ratios for each lab
+
+for (i in 1:lab_count_darley) {
+  
+  darley_mc3_lor[i, ] <- odds_calc(
+    
+    ID = unique(darley$lab)[i], 
+    
+    x = darley$scenario[darley$lab == i], 
+    y = darley$attribution[darley$lab == i], 
+    
     cond_1 = "operable", 
     cond_2 = "inoperable"
     
+  )
+  
+}
+
+# Export Calculated Effect Sizes --------------------------------------
+
+## If the data directory does not exist, it will be necessary to create it
+
+if (!file.exists("./data/darley_effects/")) {
+  
+  dir.create("./data/darley_effects/")
+  
+} 
+
+## Hypothesis 1
+
+if (!file.exists("./data/darley_effects/darley_h1_smd.csv")) {
+  
+  write.csv(
+    darley_h1_smd,
+    "./data/darley_effects/darley_h1_smd.csv",
+    row.names = FALSE
+  )
+  
+}
+
+## Hypothesis 2
+
+if (!file.exists("./data/darley_effects/darley_h2_smd.csv")) {
+  
+  write.csv(
+    darley_h2_smd,
+    "./data/darley_effects/darley_h2_smd.csv",
+    row.names = FALSE
+  )
+  
+}
+
+## Hypothesis 3
+
+if (!file.exists("./data/darley_effects/darley_h3_smd.csv")) {
+  
+  write.csv(
+    darley_h3_smd,
+    "./data/darley_effects/darley_h3_smd.csv",
+    row.names = FALSE
+  )
+  
+}
+
+## Manipulation check 1
+
+if (!file.exists("./data/darley_effects/darley_mc1_lor.csv")) {
+  
+  write.csv(
+    darley_mc1_lor,
+    "./data/darley_effects/darley_mc1_lor.csv",
+    row.names = FALSE
+  )
+  
+}
+
+## Manipulation check 2
+
+if (!file.exists("./data/darley_effects/darley_mc2_lor.csv")) {
+  
+  write.csv(
+    darley_mc1_lor,
+    "./data/darley_effects/darley_mc2_lor.csv",
+    row.names = FALSE
+  )
+  
+}
+
+## Manipulation check 3
+
+if (!file.exists("./data/darley_effects/darley_mc3_lor.csv")) {
+  
+  write.csv(
+    darley_mc1_lor,
+    "./data/darley_effects/darley_mc3_lor.csv",
+    row.names = FALSE
   )
   
 }
