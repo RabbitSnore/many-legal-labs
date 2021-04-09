06350e36cea52d02d0e31d4d00595ab0e114643f
Lina Nyström <lina.nystrom@psy.gu.se>
2021-04-09 15:26:54 +0200

    Hypothesis 5 added to analysis

--- scripts/analyze/correll_analysis.R
+++ scripts/analyze/correll_analysis.R
@@ -29,6 +29,7 @@ correll_h1_smd <- read.csv("./data/correll_effects/correll_h1_smd.csv")
 correll_h2_smd <- read.csv("./data/correll_effects/correll_h2_smd.csv")
 correll_h3_smd <- read.csv("./data/correll_effects/correll_h3_smd.csv")
 correll_h4_smd <- read.csv("./data/correll_effects/correll_h4_smd.csv")
+correll_h5_smd <- read.csv("./data/correll_effects/correll_h5_smd.csv")
 
 # Import original effects
 
@@ -101,8 +102,8 @@ correll_h3_meta <- rma(
 
 correll_h3_forest <- 
   forest_plot_smd(
-    meta_analysis = correll_h2_meta, 
-    replication_data = correll_h2_smd, 
+    meta_analysis = correll_h3_meta, 
+    replication_data = correll_h3_smd, 
     title = "Correll et al (2002), Hypothesis 3",
     study_color = correll_color_1,
     org_d = correll_org[correll_org$hypothesis == "h3", ]$d, 
@@ -132,4 +133,28 @@ correll_h4_forest <-
     org_d = correll_org[correll_org$hypothesis == "h4", ]$d, 
     org_ci_lower = correll_org[correll_org$hypothesis == "h4", ]$ci_lower, 
     org_ci_upper = correll_org[correll_org$hypothesis == "h4", ]$ci_upper
+  )
+
+# HYPOTHESIS 5 --------------------------------------------------------
+
+## Random effects meta-analysis
+
+correll_h5_meta <- rma(
+  yi = d, 
+  vi = var,
+  data = correll_h5_smd,
+  method = "REML"
+)
+
+## Forest plot
+
+correll_h5_forest <- 
+  forest_plot_smd(
+    meta_analysis = correll_h5_meta, 
+    replication_data = correll_h5_smd, 
+    title = "Correll et al (2002), Hypothesis 5",
+    study_color = correll_color_1,
+    org_d = correll_org[correll_org$hypothesis == "h5", ]$d, 
+    org_ci_lower = correll_org[correll_org$hypothesis == "h5", ]$ci_lower, 
+    org_ci_upper = correll_org[correll_org$hypothesis == "h5", ]$ci_upper
   )
